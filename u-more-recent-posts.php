35620a3bbe6ae4f1b8bf3609f1dc641a51d8b4e1
taehan <taehan@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-13 05:03:40 +0000

    git-svn-id: https://plugins.svn.wordpress.org/u-more-recent-posts/tags/1.0@344711 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- u-more-recent-posts.php
+++ u-more-recent-posts.php
@@ -1,12 +1,12 @@
 <?php
 /* 
 Plugin Name: U More Recent Posts
-Plugin URI: http://urlless.com/u-more-recent-posts/
+Plugin URI: http://urlless.com/wordpress-plugin-u-more-recent-posts/
 Description: Based on Wordpress core "Recent Posts" widget, this plugin is redesigned to make it possible to navigate more recent posts without refreshing screen.
 Version: 1.0 
 Author: Taehan Lee
 Author URI: http://urlless.com
-*/
+*/ 
 
 global $wp_version;
 if (version_compare($wp_version, "2.8", "<")) wp_die("This plugin requires WordPress version 2.8 or higher.");
@@ -18,13 +18,13 @@ class UMoreRecentPosts {
 	
 	function UMoreRecentPosts(){
 		$this->plugin_url = plugin_dir_url(__FILE__);
-		add_action( 'init', array(&$this, 'init') ); 
-		add_action( 'widgets_init', array(&$this, 'widgets_init') ); 
+		add_action( 'init', array(&$this, 'scripts_action') ); 
 		add_action( 'wp_ajax_umrp-ajax', array(&$this, 'ajax') );
 		add_action( 'wp_ajax_nopriv_umrp-ajax', array(&$this, 'ajax') );
+		add_action( 'widgets_init', array(&$this, 'register_widget_action') ); 
 	}
 	
-	function init() { 
+	function scripts_action() { 
 		if ( ! is_admin() ) {
 			wp_enqueue_script( 'jquery' ); 
 			wp_enqueue_style( 'umrp_style', $this->plugin_url.'u-more-recent-posts.css');
@@ -36,44 +36,65 @@ class UMoreRecentPosts {
 		}
 	}
 	
-	function widgets_init() { 
-		register_widget( 'UMoreRecentPostsWidget' ); 
-	}
-	
 	function ajax() {
 		check_ajax_referer( 'umrp_nonce' );
 		
-		$args = array(
-			'posts_per_page' => intval( $_POST['posts_per_page'] ) ? $_POST['posts_per_page'] : 5,
-			'paged' => $_POST['paged'], 
-			'nopaging' => 0, 
-			'post_status' => 'publish', 
-			'caller_get_posts' => 1
-		);
-		if( ! empty($_POST['exclude']) ) $args['category__not_in'] = explode(',', $_POST['exclude']);
-		if( ! empty($_POST['include']) ) $args['category__in'] = explode(',', $_POST['include']);
-		
-		$r = new WP_Query($args);
-		if($r->have_posts()): 
-			echo '<ul>';
-			while($r->have_posts()): $r->the_post();
-			?>
-			<li><a href="<?php the_permalink() ?>" title="<?php echo esc_attr(get_the_title() ? get_the_title() : get_the_ID()); ?>"><?php if ( get_the_title() ) the_title(); else the_ID(); ?></a></li>
-			<?php
-			endwhile; 
-			echo '</ul>';
+		switch( $_POST['action_type'] ):
 			
-			if( $pager = $this->pager( array(
-				'posts_per_page' => $args['posts_per_page'],
-				'paged' => $args['paged'],
-				'found_posts' => $r->found_posts
-			) ) ): 
-			?>
-			<div class="umrp-nav"><?php echo $pager; ?></div>
-			<?php
-			endif;
-		endif;
-		die();
+			case 'get_option':
+				$opts = get_option('widget_umrp');
+				$opt = $opts[intval($_POST['id'])];
+				die( json_encode($opt) );
+				break;
+				
+			case 'get_list':
+				$args = array(
+					'posts_per_page' => intval( $_POST['number'] ) ? $_POST['number'] : 5,
+					'paged' => $_POST['paged'], 
+					'nopaging' => 0, 
+					'post_status' => 'publish', 
+					'caller_get_posts' => 1
+				);
+				if( ! empty($_POST['exclude']) ) $args['category__not_in'] = explode(',', $_POST['exclude']);
+				if( ! empty($_POST['include']) ) $args['category__in'] = explode(',', $_POST['include']);
+				
+				$r = new WP_Query($args);
+				if($r->have_posts()): 
+					echo '<ul>';
+					while($r->have_posts()): $r->the_post();
+					
+					$title = get_the_title() ? get_the_title() : get_the_ID();
+					$title = apply_filters('the_title', $title);
+					$title_attr = esc_attr($title);
+					
+					$word_limit = isset($_POST['length']) ? intval($_POST['length']) : 0;
+					if( $word_limit>0 ){
+						$words = explode(' ',$title);
+						if(count($words) > $word_limit) {
+							array_splice($words, $word_limit);
+		    				$title = implode(' ', $words) . '&hellip;';
+		    			}
+	    			}
+					?>
+					<li><a href="<?php the_permalink() ?>" title="<?php echo $title_attr; ?>"><?php echo $title; ?></a></li>
+					<?php
+					endwhile; 
+					echo '</ul>';
+					
+					if( $pager = $this->pager( array(
+						'posts_per_page' => $args['posts_per_page'],
+						'paged' => $args['paged'],
+						'found_posts' => $r->found_posts
+					) ) ): 
+					?>
+					<div class="umrp-nav"><?php echo $pager; ?></div>
+					<?php
+					endif;
+				endif;
+				die();
+				break;
+				
+		endswitch;
 	}
 	
 	function pager($args) {
@@ -120,6 +141,10 @@ class UMoreRecentPosts {
 		$ret .= $currentpage==$i ? "<em>$i</em>" : "<a href='#'>$i</a>";
 		return $ret;
 	}
+	
+	function register_widget_action() { 
+		register_widget( 'UMoreRecentPostsWidget' ); 
+	}
 }
 
 
@@ -130,22 +155,25 @@ class UMoreRecentPostsWidget extends WP_Widget {
 	function UMoreRecentPostsWidget() {
 		$this->plugin_url = plugin_dir_url(__FILE__);
 		$opts = array( 'classname' => 'widget_umrp' ); 
-		$this->WP_Widget( 'umrp', __('More Recent Posts', 'umrp'), $opts ); 
+		$this->WP_Widget( 'umrp', __('More Recent Posts', 'umrp'), $opts );
 	}
 	
 	function form($instance) { 
-		$defaults = array( 'title'=>__('Recent Posts', 'umrp'), 'number'=>'5', 'exclude'=>'', 'include'=>'' ); 
+		$defaults = array( 'title'=>__('Recent Posts', 'umrp'), 'number'=>'5', 'exclude'=>'', 'include'=>'', 'length'=>'', 'effect'=>'' ); 
 		$instance = wp_parse_args( (array) $instance, $defaults ); 
 		$title = esc_attr( $instance['title'] ); 
 		$number = esc_attr( $instance['number'] ); 
 		$exclude = esc_attr( $instance['exclude'] ); 
 		$include = esc_attr( $instance['include'] ); 
+		$length = esc_attr( $instance['length'] ); 
+		$effect = esc_attr( $instance['effect'] ); 
+		$effects = array('none'=>'none', 'fadein'=>'Fade In', 'slidedown'=>'Slide Down');
 		?>
 		<p>
 			<label><?php _e('Title', 'umrp'); ?>:</label>
 			<input name="<?php echo $this->get_field_name('title'); ?>" value="<?php echo $title; ?>" type="text" class="widefat" /></p> 
 		<p>
-			<label><?php _e('Number of list to show per page', 'umrp'); ?>: </label>
+			<label><?php _e('Number of list to show', 'umrp'); ?>: </label>
 			<input name="<?php echo $this->get_field_name('number'); ?>" value="<?php echo $number; ?>" type="text" size="2" />
 		</p>
 		<p>
@@ -158,6 +186,22 @@ class UMoreRecentPostsWidget extends WP_Widget {
 			<input name="<?php echo $this->get_field_name('include'); ?>" value="<?php echo $include; ?>" type="text" class="widefat" />
 			<br><small><?php _e('Separated by commas', 'umrp'); ?>.</small>
 		</p>
+		<p>
+			<label><?php _e('Title Length', 'umrp'); ?>: </label>
+			<input name="<?php echo $this->get_field_name('length'); ?>" value="<?php echo $length; ?>" type="text" size="3" /> 
+			<?php _e('words', 'umrp'); ?>
+		</p>
+		<p>
+			<label><?php _e('Effect', 'umrp'); ?>: </label>
+			<select name="<?php echo $this->get_field_name('effect'); ?>">
+				<?php foreach($effects as $key=>$val){
+					$selected = $effect==$key ? 'selected="selected"' : '';
+					?>
+					<option value="<?php echo $key; ?>" <?php echo $selected; ?>><?php echo $val; ?></option>
+					<?php
+				} ?>
+			</select>
+		</p>
 		<?php 
 	} 
 	
@@ -167,6 +211,8 @@ class UMoreRecentPostsWidget extends WP_Widget {
 		$instance['number'] = strip_tags($new_instance['number']);
 		$instance['exclude'] = strip_tags($new_instance['exclude']);
 		$instance['include'] = strip_tags($new_instance['include']);
+		$instance['length'] = strip_tags($new_instance['length']);
+		$instance['effect'] = strip_tags($new_instance['effect']);
 		return $instance; 
 	} 
 	
@@ -176,15 +222,10 @@ class UMoreRecentPostsWidget extends WP_Widget {
 		echo $before_widget;
 		if( $title ) echo $before_title . $title . $after_title;
 		?>
-		<img class="umrp-status" src="<?php echo $this->plugin_url; ?>images/loading.gif" />
-		<div class="umrp-container"></div>
-		<script>
-			jQuery("#<?php echo $widget_id; ?>").UMoreRecentPostsWidget({
-				number:"<?php echo esc_js($instance['number']); ?>", 
-				exclude:"<?php echo esc_js($instance['exclude']); ?>", 
-				include:"<?php echo esc_js($instance['include']); ?>"
-			});
-		</script>
+		<div class="umrp-container">
+			<img class="umrp-status" src="<?php echo $this->plugin_url; ?>images/loading.gif" />
+			<div class="umrp-content"></div>
+		</div>
 		<?php
         echo $after_widget; 
 	}
